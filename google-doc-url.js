cc63ad4f400d51fa866521621e97f302cd3d938c
venturaEffect <29057173+venturaEffect@users.noreply.github.com>
2019-09-26 11:45:32 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZvZhMeK8LBIlY8VsyULCr1AyKhbkx_7h07WH1NoUaU0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10QZQobzA3tyHoZ9ZZw7uRU-6CVv9D7nN4kSczjlaDtQ/edit#gid=0';
